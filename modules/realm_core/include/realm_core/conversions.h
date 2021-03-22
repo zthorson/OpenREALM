30991e36e71e2b4c3344c7a871211776eb347f80
Zach Thorson <zachthorson@gmail.com>
2021-03-22 18:34:41 -0500

    Track average position and heading to improve projection if/when tracking is lost

--- modules/realm_core/include/realm_core/conversions.h
+++ modules/realm_core/include/realm_core/conversions.h
@@ -3,6 +3,7 @@
 #ifndef PROJECT_GIS_CONVERSIONS_H
 #define PROJECT_GIS_CONVERSIONS_H
 
+#include <opencv2/opencv.hpp>
 #include <gdal/ogr_spatialref.h>
 
 #include <realm_core/utm32.h>
@@ -32,6 +33,26 @@ WGSPose convertToWGS84(const UTMPose &utm);
  * @param oSRS Spatial reference to be patched depending on the installed GDAL version
  */
 void initAxisMappingStrategy(OGRSpatialReference *oSRS);
+
+}
+
+namespace pose
+{
+    /*! TODO: Einbaurichtung implementieren?
+ * @brief Function to compute a 3x3 rotation matrix based on heading data. It is assumed, that the camera is pointing
+ * downwards and the heading roughly aligns with the camera's yaw axis.
+ * @param heading Magnetic heading of the camera / UAV
+ * @return 3x3 rotation matrix
+ */
+    cv::Mat computeOrientationFromHeading(double heading);
+
+/*! TODO: Make this more general to get full yaw/pitch/roll
+ * @brief Get yaw from a 3x3 rotation matrix It is assumed, that the camera is pointing
+ * downwards and the heading roughly aligns with the camera's yaw axis.
+ * @param orientation The orientation matrix to use
+ * @return The yaw in degrees
+ */
+    double getHeadingFromOrientation(const cv::Mat& orientation);
 
 }
 }
